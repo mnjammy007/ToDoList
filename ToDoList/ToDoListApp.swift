14302a6c810542be739d4d992764ad84dc4b7068
mnjammy007 <naseem4954r@gmail.com>
2025-01-29 11:33:15 +0530

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Apple on 29/01/25.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
